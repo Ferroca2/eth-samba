c37ae0488f6c55d3b12862f6b40c8fd5b05477c5
Rafael450 <rafaelcamargo633@gmail.com>
2024-03-23 18:57:40 -0300

    feat: created proposal endpoints and likes endpoints

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -55,6 +55,119 @@ app.get('/protocol', async (
 });
 
 
+app.post('/proposal', async (
+  req: Request, 
+  res: Response
+) => {
+  const { protocol_id, creator, title, description } = req.body;
+
+  const proposal = await prisma.proposal.create({
+    data: {
+      protocol_id: parseInt(protocol_id),
+      creator,
+      title, 
+      description
+    },
+  });
+
+  return res.status(201).json(proposal);
+});
+
+app.get('/proposal', async (
+  req: Request,
+  res: Response,
+) => {
+
+  const proposals = await prisma.proposal.findMany({
+    select: {
+      id: true,
+      protocol_id: true,
+      title: true,
+      description: true,
+    }
+  });
+
+  return res.status(200).json(proposals); 
+});
+
+app.get('/proposal/:proposal_id', async (
+  req: Request,
+  res: Response,
+) => {
+  const { protocol_id } = req.params;
+  const proposals = await prisma.proposal.findMany({
+    where: {
+      protocol_id: parseInt(protocol_id as string),
+      on_chain: false,
+    },
+    select: {
+      id: true,
+      protocol_id: true,
+      title: true,
+      description: true,
+      comments: {
+        select: {
+          id: true,
+          creator: true,
+          content: true,
+        }
+      }
+    }
+  });
+
+  const commentIds = proposals.map((proposal) => proposal.comments.map((comment) => comment.id));
+  const likesPromises = commentIds.flat().map(
+    (commentId) => prisma.like.count({ 
+      where: { 
+        comment_id: commentId 
+      } 
+    })
+  );
+
+  const likesCounts = await Promise.all(likesPromises);
+
+  proposals.forEach((proposal, index) => {
+    proposal.comments.forEach((comment:any, commentIndex) => {
+      comment.likesCount = likesCounts[index * proposal.comments.length + commentIndex];
+    });
+  });
+
+  return res.status(200).json(proposals); 
+});
+
+app.post('/like', async (
+  req: Request, 
+  res: Response
+) => {
+  const { comment_id, address } = req.body;
+
+  const like = await prisma.like.create({
+    data: {
+      comment_id: parseInt(comment_id),
+      address,
+    },
+  });
+
+  return res.status(201).json(like);
+});
+
+app.delete("/like/:id", async (
+  req: Request, 
+  res: Response
+) => {
+  const { comment_id, address } = req.body;
+  await prisma.like.delete({ 
+    where: { 
+      address_comment_id: {
+        address,
+        comment_id: parseInt(comment_id),
+      }
+    } 
+  });
+
+  return res.sendStatus(204);
+});
+
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
