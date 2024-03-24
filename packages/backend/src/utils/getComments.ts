85f7260d24d6e3a193d0a441bc9392380c83e9c7
Rafael450 <rafaelcamargo633@gmail.com>
2024-03-24 05:32:18 -0300

    feat: implemented comment filter by AI

--- packages/backend/src/utils/getComments.ts
+++ packages/backend/src/utils/getComments.ts
@@ -1,16 +1,67 @@
 import { prisma } from "./database";
+import axios from "axios";
+import { getOpenAIFilter } from "./openai";
+import { filterPrompt } from "./prompt";
+import { parse } from "path";
 
 export const getComments = async (proposal_id: number, max_amount = 20): Promise<Array<{ id: string, comment: string }>> =>  {
-  const comments = await prisma.comment.findMany({
+  const proposal = await prisma.proposal.findUnique({
     where: {
-      proposal_id,
+      id: proposal_id,
     },
     select: {
-      content: true,
+      description: true,
+      title: true,
+      comments: true,
+      protocol: {
+        select: {
+          description: true,
+        },
+      }
+      
     }
   });
+
+  const getCreatorAge = async (address: string) => {
+    try {
+      const response = await axios.get(`https://explorer.old.testnet.aurora.dev/api/v2/addresses/${address}/transactions`);
+      const creatorInfo = response.data;
+      console.log(creatorInfo);
   
-  return comments.map((comment, index) => ({
+      const age = creatorInfo.items[creatorInfo.items.length - 1];
+  
+      const date = new Date(age.timestamp);
+  
+      return date;
+    } catch (error) {
+      console.error("Error fetching creator info:", error);
+      return new Date(Date.now());
+    }
+  };
+
+  const commentsWithAge = (await Promise.all(proposal!.comments.map(async (comment) => {
+    const age = await getCreatorAge(comment.creator);
+    return {
+      ...comment,
+      first_transaction: age,
+    };
+  }))).map((comment) => ({
+    id: comment.id,
+    content: comment.content,
+    first_transaction: comment.first_transaction,
+  }));
+
+  const filtered = await getOpenAIFilter(filterPrompt(proposal!.protocol.description, proposal!.title, proposal!.description, commentsWithAge));
+  console.log(filtered);
+
+  const filterJSON = JSON.parse(filtered!).rejected_ids.map((id: number) => id);
+  console.log(filterJSON)
+  
+
+  const filteredComments = commentsWithAge.filter((comment) => !filterJSON.includes(comment.id));
+  
+  
+  return filteredComments.map((comment, index) => ({
     id: index.toString(),
     comment: comment.content,
   })).slice(0, max_amount);
--- packages/backend/src/utils/prompt.ts
+++ packages/backend/src/utils/prompt.ts
@@ -28,3 +28,36 @@ Please, apply the suggested changes from the feedback analysis to the text of th
 }
 \`\`\`
 `;
+
+export const filterPrompt = (
+  protocol_description: string,
+  title: string,
+  description: string,
+  comments: Array<{ id: number, content: string, first_transaction: Date }>
+  ) => `
+  Given the series of comments listed below, which were made in response to a generic proposal, your task is to identify which comments do not offer constructive or relevant contributions to the enhancement of the proposal. Each comment is identified by a unique ID for easy reference.
+
+  \`\`\`json
+  "initial_propose": {
+          "protocol_description": "${protocol_description}",
+          "title": "${title}",
+          "description": "${description}"
+      },
+      "comments": ${JSON.stringify(comments, null, 2)}
+  }
+  \`\`\`
+  
+  Your analysis should focus on distinguishing comments that do not effectively add value to the discussion on proposal improvements. After reviewing, organize your response in JSON format, pointing out the IDs of the comments you consider should be rejected, as they do not directly contribute to the enhancement of the proposal.
+  
+  Structure your response following the JSON format example below:
+  
+  \`\`\`json
+  {
+    'rejected_ids': [
+      {'id': \${ID OF REJECTED COMMENT 1}},
+      {'id': \${ID OF REJECTED COMMENT 2}},
+      ...
+    ]
+  }
+  \`\`\`
+  `;
