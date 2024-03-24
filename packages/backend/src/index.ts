7db80e32d7bee167e0f23603bc1d5a858bc11a23
Rafael450 <rafaelcamargo633@gmail.com>
2024-03-23 22:52:49 -0300

    fix: fixed error handling and made protocol titles unique

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -67,28 +67,32 @@ app.post('/protocol', async (
     address,
     image_url,
   } = req.body;
-  console.log(id, title, address, image_url)
+  
+  try {
+    const existingProtocol = await prisma.protocol.findUnique({
+      where: {
+        id: parseInt(id),
+      },
+    });
 
-  const existingProtocol = await prisma.protocol.findUnique({
-    where: {
-      id: parseInt(id),
-    },
-  });
-
-  if (existingProtocol) {
-    return res.status(400).json({ error: 'Protocol ID already exists' });
-  }
-
-  await prisma.protocol.create({
-    data: {
-      id,
-      title,
-      address,
-      image_url,
+    if (existingProtocol) {
+      return res.status(400).json({ error: 'Protocol ID already exists' });
     }
-  });
 
-  return res.sendStatus(201); 
+    await prisma.protocol.create({
+      data: {
+        id,
+        title,
+        address,
+        image_url,
+      }
+    });
+
+    return res.sendStatus(201); 
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ error: 'Error creating protocol' });
+  }
 });
 
 app.get('/protocol', async (
@@ -97,17 +101,22 @@ app.get('/protocol', async (
 ) => {
   /* #swagger.tags = ['Protocol']
     #swagger.description = 'Get all Protocols' */
-  
-  const protocols = await prisma.protocol.findMany({
-    select: {
-      id: true,
-      title: true,
-      address: true,
-      image_url: true,
-    }
-  });
 
-  return res.status(200).json(protocols); 
+  try {  
+    const protocols = await prisma.protocol.findMany({
+      select: {
+        id: true,
+        title: true,
+        address: true,
+        image_url: true,
+      }
+    });
+
+    return res.status(200).json(protocols);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ error: 'Error fetching protocols' });
+  }
 });
 
 
@@ -147,35 +156,49 @@ app.post('/proposal', async (
     */
   const { protocol_id, creator, title, description } = req.body;
 
-  const proposal = await prisma.proposal.create({
-    data: {
-      protocol_id: parseInt(protocol_id),
-      creator,
-      title, 
-      description
-    },
-  });
+  try {
+    const proposal = await prisma.proposal.create({
+      data: {
+        protocol_id: parseInt(protocol_id),
+        creator,
+        title, 
+        description
+      },
+    });
 
-  return res.status(201).json(proposal);
+    return res.status(201).json(proposal);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ error: 'Error creating proposal' });
+  }
 });
 
-app.get('/proposal', async (
+app.get('/protocol/:protocol_id', async (
   req: Request,
   res: Response,
 ) => {
-  /* #swagger.tags = ['Proposal']
-    #swagger.description = 'Get all Proposals' */
+  /* #swagger.tags = ['Protocol']
+    #swagger.description = 'Get info from a protocol' */
 
-  const proposals = await prisma.proposal.findMany({
-    select: {
-      id: true,
-      protocol_id: true,
-      title: true,
-      description: true,
-    }
-  });
+  const { protocol_id } = req.params;
+    
+  try {
+    const proposals = await prisma.protocol.findMany({
+      where: {
+        id: parseInt(protocol_id as string),
+      },
+      select: {
+        id: true,
+        title: true,
+        proposals: true
+      }
+    });
 
-  return res.status(200).json(proposals); 
+    return res.status(200).json(proposals);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ error: 'Error fetching proposals' });
+  }
 });
 
 app.get('/proposal/:proposal_id', async (
@@ -185,45 +208,52 @@ app.get('/proposal/:proposal_id', async (
   /* #swagger.tags = ['Proposal']
     #swagger.description = 'Get a Proposal by ID' */
 
-  const { protocol_id } = req.params;
-  const proposals = await prisma.proposal.findMany({
-    where: {
-      protocol_id: parseInt(protocol_id as string),
-      on_chain: false,
-    },
-    select: {
-      id: true,
-      protocol_id: true,
-      title: true,
-      description: true,
-      comments: {
-        select: {
-          id: true,
-          creator: true,
-          content: true,
+  const { proposal_id } = req.params;
+
+  try {
+    const proposal = (await prisma.proposal.findUnique({
+      where: {
+        id: parseInt(proposal_id as string),
+      },
+      select: {
+        id: true,
+        protocol_id: true,
+        title: true,
+        description: true,
+        comments: {
+          select: {
+            id: true,
+            creator: true,
+            content: true,
+          }
         }
       }
+    }));
+
+    if(!proposal) {
+      return res.status(404).json({ error: 'Proposal not found' });
     }
-  });
 
-  const commentIds = proposals.map((proposal) => proposal.comments.map((comment) => comment.id));
-  const likesPromises = commentIds.flat().map(
-    (commentId) => prisma.like.count({ 
-      where: { 
-        comment_id: commentId 
-      } 
-    })
-  );
+    const commentIds = proposal.comments.map((comment) => comment.id);
+    const likesPromises = commentIds.flat().map(
+      (commentId) => prisma.like.count({ 
+        where: { 
+          comment_id: commentId 
+        } 
+      })
+    );
 
-  const likesCounts = await Promise.all(likesPromises);
+    const likesCounts = await Promise.all(likesPromises);
 
-  proposals.forEach((proposal, index) => {
     proposal.comments.forEach((comment:any, commentIndex) => {
-      comment.likesCount = likesCounts[index * proposal.comments.length + commentIndex];
+      comment.likesCount = likesCounts[commentIndex];
     });
-  });
 
-  return res.status(200).json(proposals); 
+    return res.status(200).json(proposal); 
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ error: 'Error fetching proposal' });
+  }
 });
 
 app.post('/like', async (
@@ -255,22 +285,28 @@ app.post('/like', async (
     */
   const { comment_id, address } = req.body;
 
-  const like = await prisma.like.create({
-    data: {
-      comment_id: parseInt(comment_id),
-      address,
-    },
-  });
 
-  return res.status(201).json(like);
+  try{
+    const like = await prisma.like.create({
+      data: {
+        comment_id: parseInt(comment_id),
+        address,
+      },
+    });
+
+    return res.status(201).json(like);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ error: 'Error creating like' });
+  }
 });
 
-app.delete("/like/:id", async (
-  req: Request, 
-  res: Response
+app.post('/comment', async (
+  req: Request,
+  res: Response,
 ) => {
-  /* #swagger.tags = ['Like']
-    #swagger.description = 'Delete a Like' */
+  /* #swagger.tags = ['Comment']
+    #swagger.description = 'Create a Comment' */
 
   /* #swagger.requestBody = {
         "@content": { 
@@ -278,31 +314,38 @@ app.delete("/like/:id", async (
             schema: {
               type: 'object',
               properties: {
-                comment_id: {
+                proposal_id: {
                   type: "number",
                   example: "2"
                 },
-                address: {
+                creator: {
                   type: "string",
                   default: "0x1234567890123456789012345678901234567890",
+                },
+                content: {
+                  type: "string",
+                  default: "This is a comment",
                 },
               }
             }
           }
         }
       }
-    */  
-  const { comment_id, address } = req.body;
-  await prisma.like.delete({ 
-    where: { 
-      address_comment_id: {
-        address,
-        comment_id: parseInt(comment_id),
-      }
-    } 
-  });
-
-  return res.sendStatus(204);
+  */
+  const { proposal_id, creator, content } = req.body;
+  try {
+    const comment = await prisma.comment.create({
+      data: {
+        proposal_id: proposal_id,
+        creator,
+        content,
+      },
+    });
+    return res.status(201).json(comment);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ error: 'Error creating comment' });
+  }
 });
 
 
